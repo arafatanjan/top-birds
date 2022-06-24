adcc1869d98a19acf0598755e84038c46f238d06
arafatanjan <arafatanjan@gmail.com>
2022-06-24 17:49:50 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Directors></Directors>
+      <Header></Header>
+
     </div>
   );
 }
+
+function Directors() {
+  return (
+    <div>
+      {/* <h2> directors </h2> */}
+    </div>
+  )
+}
+
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h2> Top Bird List</h2>
+            <h3>The following is a non-definitive list of Birds, a group of warm-blooded vertebrates constituting the class Aves</h3>
+        </div>
+    );
+};
+
+export default Header;
